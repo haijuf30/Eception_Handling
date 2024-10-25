189604f42698c904adb810996e3c418ed6597b87
haijuf30 <haikal.aljufri30tif@gmail.com>
2024-10-25 10:35:36 +0700

    Case 2 exeption handling dalam loop

--- src/ParseInts.java
+++ src/ParseInts.java
@@ -9,13 +9,13 @@ public class ParseInts {
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
         
-        try {
-            while (scanLine.hasNext()) {
+        while (scanLine.hasNext()) {
+            try {
                 val = Integer.parseInt(scanLine.next());
                 sum += val;
+            } catch (NumberFormatException e) {
+                // Empty catch block
             }
-        } catch (NumberFormatException e) {
-            // Empty catch block
         }
         
         System.out.println("The sum of the integers on this line is " + sum);
