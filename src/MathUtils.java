e4d47cd59e3c20602cfb807e61c50d5bb54673f9
haijuf30 <haikal.aljufri30tif@gmail.com>
2024-10-25 22:07:44 +0700

    Add negative check

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -1,5 +1,10 @@
 public class MathUtils {
-    public static int factorial(int n) {
+    public static int factorial(int n) throws IllegalArgumentException {
+        // Check for negative input
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+        
         int fac = 1;
         for (int i=n; i>0; i--)
             fac *= i;
